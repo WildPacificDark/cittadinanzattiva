8341a4dedf6bf4413d5ebbc5eb0d263a619fbd90
Elia <45065106+WildPacificDark@users.noreply.github.com>
2019-05-08 23:26:43 +0200

    Update general.php

--- pages/general.php
+++ pages/general.php
@@ -1,65 +1,61 @@
   <?php 
-	session_start();
-	
-    if(isset($_GET['id'])) $page = $_GET['id'];
-    if(isset($_GET['loginStatus'])) $loginStatus = $_GET['loginStatus'];
-    else $loginStatus = true;
-    
-	//FORM DI LOGIN	
+  session_start();
+  if(isset($_GET['id'])) {
+	$page = $_GET['id'];
+  }
+  if(isset($_SESSION["userId"])) {
+    //FORM DI LOGIN	
     if($page == "login") {
-		require_once '../html/header/headerLogin.html';
-		require_once '../html/nav/navCommon.html';
-		echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; <strong>Accedi</strong></p>
+      if(isset($_GET['loginStatus'])) {
+        $loginStatus = $_GET['loginStatus'];
+      } else { 
+        $loginStatus = true;
+      }
+      require_once '../html/header/headerLogin.html';
+      require_once '../html/nav/navCommon.html';
+      echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; <strong>Accedi</strong></p>
+            </div>';
+      require '../html/body/bodyLoginTop.html';
+      //IN CASO DI DATI SBAGLIATI MOSTRA MESSAGGIO DI ERRORE
+      if($loginStatus == false) {
+        echo '<p class="errorField" id="wrongInfo">Nome utente o Password Errati</p>
+                 ';
+      }
+      require_once '../html/body/bodyLoginBottom.html';
+    } else if($page == "signup") {  //FORM DI REGISTRAZIONE
+        require_once '../html/header/headerSignUp.html';
+        require_once '../html/nav/navCommon.html';
+        echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; <strong>Registrati</strong></p>
+            </div>';
+        require_once '../html/body/bodySignUp.html';
+    } else if($page == "contatti") { //FORM DI CONTATTO
+        require_once '../html/header/headerFormContatti.html';
+        require_once '../html/nav/navCommon.html';
+        echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; Contattaci &rsaquo; <strong>Nuova Richiesta</strong></p>
+            </div>';
+        require_once '../html/body/bodyFormContatti.html';
+    } else if($page == "report") { //FORM CREA SEGNALAZIONE
+        require_once '../html/header/headerCreateReport.html';
+        require_once '../html/nav/navCommon.html';
+        echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; Servizi &rsaquo; <strong>Crea Segnalazione</strong></p>
 			</div>';
-        require '../html/body/bodyLoginTop.html';
-		//IN CASO DI DATI SBAGLIATI MOSTRA MESSAGGIO DI ERRORE
-        if($loginStatus == false) {
-		    echo '<p class="errorField" id="wrongInfo">Nome utente o Password Errati</p>
-		         ';
-        }
-        require_once '../html/body/bodyLoginBottom.html';
+      require_once '../html/body/bodyCreateReport.html';
+    } else if($page == "viewReport") { //FORM VISUALIZZA SEGNALAZIONE
+        require_once '../html/header/headerViewReport.html';
+        require_once '../html/nav/navCommon.html';
+        echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; Servizi &rsaquo; Elenco delle segnalazioni &rsaquo; 
+                <strong>Visualizza Segnalazione</strong></p>
+            </div>';
+        $reportId = $_GET['reportId'];
+        require_once '../php/script/viewReportScript.php';
+        loadSelectedReport($reportId);
     }
-	//FORM DI REGISTRAZIONE
-    else if($page == "signup") {
-		require_once '../html/header/headerSignUp.html';
-		require_once '../html/nav/navCommon.html';
-		echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; <strong>Registrati</strong></p>
-			</div>';
-		require_once '../html/body/bodySignUp.html';
-	}
-	//FORM DI CONTATTO
-    else if($page == "contatti") {
-		require_once '../html/header/headerFormContatti.html';
-		require_once '../html/nav/navCommon.html';
-		echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; Contattaci &rsaquo; <strong>Nuova Richiesta</strong></p>
-			</div>';
-		require_once '../html/body/bodyFormContatti.html';
-		
-	}
-	//FORM CREA SEGNALAZIONE
-    else if($page == "report") {
-		require_once '../html/header/headerCreateReport.html';
-		require_once '../html/nav/navCommon.html';
-		echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; Servizi &rsaquo; <strong>Crea Segnalazione</strong></p>
-			</div>';
-		require_once '../html/body/bodyCreateReport.html';
-	}
-	//FORM VISUALIZZA SEGNALAZIONE
-	else if($page == "viewReport") 
-	{	
-		require_once '../html/header/headerViewReport.html';
-		require_once '../html/nav/navCommon.html';
-		echo '<p id="breadcrumb" class="breadcrumb">Home &rsaquo; Servizi &rsaquo; Elenco delle segnalazioni &rsaquo; 
-				<strong>Visualizza Segnalazione</strong></p>
-			</div>';
-		if(isset($_SESSION["userId"])) {
-			$reportId = $_GET['reportId'];
-			require_once '../php/script/viewReportScript.php';
-			loadSelectedReport($reportId);
-		} else {
-			require_once '../html/body/body403.html';
-		}
-	}
-    if($page == "contatti") require_once '../html/footer/footerFormContatti.html';
-	else require_once '../html/footer/footer.html';
+    if($page == "contatti") {
+      require_once '../html/footer/footerFormContatti.html';
+	} else {
+      require_once '../html/footer/footer.html';
+    }
+  } else {
+      require_once '../html/body/body403.html';
+  }
 ?>
