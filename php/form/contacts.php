d05f8fde67a424f9e598a0564789c5e5f28411cc
Elia <45065106+WildPacificDark@users.noreply.github.com>
2019-05-09 10:36:21 +0200

    Update contacts.php

--- php/form/contacts.php
+++ php/form/contacts.php
@@ -1,19 +1,23 @@
 <?php 
-	include '../database/config.php';
-	
-	if(!empty($_POST))
-	{
-		$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-		
-		$nome = $mysqli->real_escape_string($_POST['nome']);
-		$cognome = $mysqli->real_escape_string($_POST['cognome']);
-		$email = $mysqli->real_escape_string($_POST['email']);
-		$textarea = $mysqli->real_escape_string($_POST['textarea']);
-		$letta = 'No';
-		
-		$sql = "INSERT INTO richieste(Nome, Cognome, Email, Richiesta, RichiestaLetta) VALUES ('$nome', '$cognome', '$email', '$textarea', '$letta')";
-		
-		mysqli_query($mysqli, $sql);
-		
-		Header("Location: ../../pages/contatti.php");
-	}
+  //INCLUDI IL FILE CON I DATI PER LA CONNESSIONE AL DB
+  include '../database/config.php';
+  //SE LA RICHIESTA E' STATA INVIATA
+  if (!empty($_POST)) {
+    //CONNETTITI AL DB
+    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    //PRELEVA DALLA POST IL VALORE ED ESEGUI L'ESCAPE
+    $nome = $mysqli->real_escape_string($_POST['nome']);
+    $cognome = $mysqli->real_escape_string($_POST['cognome']);
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $textarea = $mysqli->real_escape_string($_POST['textarea']);
+    $letta = 'No';
+    //PREPARA LA QUERY
+    $sql = "INSERT INTO richieste(Nome, Cognome, Email, Richiesta, RichiestaLetta) VALUES ('$nome', '$cognome', '$email', '$textarea', '$letta')";
+    //VERIFICA SE LA QUERY E' STATA ESEGUITA CORRETTAMENTE
+    if (mysqli_query($mysqli, $sql)) {
+      header("Location: ../../pages/contatti.php?feed=true");
+    } else {
+      header("Location: ../../pages/contatti.php?feed=false");
+    }
+  }
+?>
