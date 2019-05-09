e2204dafdb17c221fd6400ac5d69c7336fa7b5bf
Elia <45065106+WildPacificDark@users.noreply.github.com>
2019-05-09 10:38:13 +0200

    Update Logout.php

--- php/form/Logout.php
+++ php/form/Logout.php
@@ -1,15 +1,12 @@
 <?php
-    //SCRIPT DI LOGOUT
-    
-    //INIZIA LA SESSIONE
-    session_start();
-    
-    //SE ESISTE EFFETIVAMENTE UNA SESSIONE ATTIVA PER L'UTENTE
-    if(isset($_SESSION['user_id']))
-    {
-        //DISTRUGGI LA SESSIONE
-        session_destroy();
-        //REINDIRIZZA L'UTENTE ALLA PAGINA INIZIALE
-        header("Location: ../../index.php");
-    }
+  //SCRIPT DI LOGOUT
+  //INIZIA LA SESSIONE
+  session_start();
+  //SE ESISTE EFFETIVAMENTE UNA SESSIONE ATTIVA PER L'UTENTE
+  if (isset($_SESSION['userId'])) {
+    //DISTRUGGI LA SESSIONE
+    session_destroy();
+    //REINDIRIZZA L'UTENTE ALLA PAGINA INIZIALE
+    header("Location: ../../index.php");
+  }
 ?>
